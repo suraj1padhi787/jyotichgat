326e63fc3a956c5e4f7eb8c811e7a0f3897cbc7b
suraj1padhi787 <suraj1padhi@gmail.com>
2025-04-11 13:24:29 +0530

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,21 @@
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('chat-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/chat',
-          '/css/style.css',
-          '/js/main.js',
-          '/manifest.json'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((response) => {
-        return response || fetch(e.request);
-      })
-    );
-  });
-  
+  e.waitUntil(
+    caches.open('chat-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/chat',
+        '/css/style.css',
+        '/manifest.json',
+        '/icons/app-icon.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
